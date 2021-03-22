9604eb5955d672124e90e030ea8474fb16cbc19c
TranCongMinh3006 <minhlinhtinhtp@gmail.com>
2021-03-22 19:41:23 +0700

    add cralwer

--- recomendation_system_backend/settings.py
+++ recomendation_system_backend/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'recomendation_system_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'vnexpress_test',
+        'NAME': 'vnexpressdemo',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
